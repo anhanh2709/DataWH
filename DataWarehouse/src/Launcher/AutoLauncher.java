f93229885c1d8fbb339a9e97658d84af3d1370c5
ThanhTungZ212Z <67012708+ThanhTung212Z@users.noreply.github.com>
2020-08-10 08:18:54 +0700

    Update AutoLauncher.java

--- DataWarehouse/src/Launcher/AutoLauncher.java
+++ DataWarehouse/src/Launcher/AutoLauncher.java
@@ -1,6 +1,8 @@
 package Launcher;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ETL.LoadFromLocalToStaging;
 import ETL.LoadFromSources;
@@ -13,17 +15,19 @@ import util.LogUtils;
 
 public class AutoLauncher {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//lÃ¢Ì�y ra 1 config coÌ� flag  = 1 Ä‘ÃªÌ‰ chaÌ£y tÆ°Ì£ Ä‘Ã´Ì£ng
-		// config tiÃªÌ�p theo seÌƒ tÆ°Ì£ Ä‘Ã´Ì£ng coÌ� flag = 1
+		// Lấy 1 config ra để chạy tự đông, config có flag = 1;
+		// config tiếp theo sẽ đc chuyển flag thành 1 flag vừa lấy ra sẽ chuyển thành 0 
 		Config config = ConfigUtils.getConfigAutoRun();
 		System.out.println("Running warehouse config: " + config.getConfig_name());
 		//Chay download
 		LoadFromSources LFS = new LoadFromSources();
-		// gÄƒÌ�n config cho bÆ°Æ¡Ì�c 1
+		// 
 		LFS.DownLoad(config);
+		List<String> fileNames = new ArrayList<String>();
+		fileNames = LogUtils.getFilesInLog(config.getConfig_id(), "ER");
 		//Chay extract
 		LoadFromLocalToStaging ls = new LoadFromLocalToStaging();
-		// gÄƒÌ�n config cho bÆ°Æ¡Ì�c 2
+		// gắn config cho bước 2;
 		ls.setConfigName(config.getConfig_name());
 		ReadFile rf = new ReadFile();
 		ControlDatabase cdb = new ControlDatabase();
@@ -31,14 +35,16 @@ public class AutoLauncher {
 		cdb.setTarget_db_name("staging");
 		cdb.setTable_name("config");
 		rf.setCdb(cdb);
-		// lÃ¢Ì�y file Ä‘Ã¢Ì€u tiÃªn trong log maÌ€ coÌ� config_id = configid cuÌ‰a config mÆ¡Ì�i lÃ¢Ì�y ra vaÌ€ coÌ� trang thaÌ�i laÌ€ ER
-		String file = LogUtils.getFirstFileInLog(config.getConfig_id(), "ER");
-		System.out.println("extract file : " + file);
-		ls.ExtractToDatabase(rf,file);
-		//Chay transform 
-		LoadFromStagingToWareHouse LSTW = new LoadFromStagingToWareHouse();
-		// gÄƒÌ�n config cho bÆ°Æ¡Ì�c 3
-		LSTW.load(config);
+		// chạy từng file trong danh sách file lấy ra từ log
+		for (String file : fileNames) {
+			System.out.println("extract file : " + file);
+			ls.ExtractToDatabase(rf,file);
+			//Chay transform 
+			LoadFromStagingToWareHouse LSTW = new LoadFromStagingToWareHouse();
+			// gắn config cho bước 3;
+			LSTW.load(config);
+		}
+		
 	}
 
 }
